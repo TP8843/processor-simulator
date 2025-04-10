8b7062f911d3cc6d4b5e387bcc7b4d55e6b59baf
Thomas Parr <github.us8r1@tpalias.com>
2025-04-10 17:18:03 +0100

    Create multi value buffer and start using them

--- app/src/main/java/org/example/Simulator.java
+++ app/src/main/java/org/example/Simulator.java
@@ -18,7 +18,7 @@ public class Simulator {
     public final Registers registers = new Registers();
 
     // Buffers
-    public final Buffer<UndecodedInstruction> fetchDecodeBuffer = new SingleValueBuffer<>();
+    public final Buffer<UndecodedInstruction> fetchDecodeBuffer = new MultiValueBuffer<>(4);
     public final Buffer<Instruction> decodeIssueBuffer = new SingleValueBuffer<>();
     public final DataBlockingBuffer decodeBranchBuffer = new DataBlockingBuffer();
 
@@ -26,7 +26,7 @@ public class Simulator {
     public final ReservationStation compareReservationStation = new ReservationStation();
 
     public final ReservationStation aguReservationStation = new ReservationStation();
-    public final Buffer<Instruction> aguLoadBuffer = new SingleValueBuffer<>();
+    public final Buffer<Instruction> aguLoadBuffer = new MultiValueBuffer<>(4);
 
     /// Initial buffers to flush for jumps and branches
     public final Flushable[] jumpBuffers = new Flushable[]{ fetchDecodeBuffer };
---
+++ app/src/main/java/org/example/processor/buffers/MultiValueBuffer.java
@@ -0,0 +1,74 @@
+package org.example.processor.buffers;
+
+import org.example.processor.instructions.Instruction;
+
+import java.util.Optional;
+
+public class MultiValueBuffer<T> implements Buffer<T>, Flushable {
+    /// Backing queue for buffer
+    private final CircularQueue<T> queue;
+
+    /// True if buffer is stalled
+    private boolean stalled = false;
+
+    public MultiValueBuffer(int size) {
+        this.queue = new CircularQueue<>(size);
+    }
+
+    @Override
+    public Optional<T> pop() {
+        if (!queue.isEmpty() && !stalled) {
+            return Optional.of(queue.dequeue().get());
+        } else
+            return Optional.empty();
+    }
+
+    @Override
+    public Optional<T> peek() {
+        if(!queue.isEmpty()) {
+            return Optional.of(queue.dequeue().get());
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean put(T value) {
+        if (queue.isFull()) return false;
+
+        queue.enqueue(value);
+        return true;
+    }
+
+    @Override
+    public boolean hasSpace() {
+        return !queue.isFull();
+    }
+
+    @Override
+    public boolean hasValue() {
+        return !stalled && !queue.isEmpty();
+    }
+
+    @Override
+    public void stall() {
+        stalled = true;
+    }
+
+    @Override
+    public void release() {
+        stalled = false;
+    }
+
+    @Override
+    public void flush() {
+        queue.flush();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Stalled: %s
+                Items: %s""", stalled ? "True" : "False", queue);
+    }
+}
